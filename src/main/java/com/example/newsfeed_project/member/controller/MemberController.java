0d68e580464c015cea697e088ad9e714878b2347
Fangsangik <hwangsangik@gmail.com>
2024-11-19 23:45:15 +0900

    refactor : "MemberController" - ResponseDto와 Controller에서 처리 할때 정적과 동적기능이 출돌해 ResponseDto부분을 제거

--- src/main/java/com/example/newsfeed_project/member/controller/MemberController.java
+++ src/main/java/com/example/newsfeed_project/member/controller/MemberController.java
@@ -2,8 +2,10 @@ package com.example.newsfeed_project.member.controller;
 
 import com.example.newsfeed_project.member.dto.MemberDto;
 import com.example.newsfeed_project.member.dto.MemberUpdateResponseDto;
+import com.example.newsfeed_project.member.dto.PasswordRequestDto;
 import com.example.newsfeed_project.member.dto.ResponseDto;
-import com.example.newsfeed_project.member.service.MemberServiceImpl;
+import com.example.newsfeed_project.member.service.MemberService;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -11,45 +13,40 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/members")
 public class MemberController {
 
-    private final MemberServiceImpl memberService;
+    private final MemberService memberService;
 
-    public MemberController(MemberServiceImpl memberService) {
+    public MemberController(MemberService memberService) {
         this.memberService = memberService;
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<ResponseDto<MemberDto>> findById(@PathVariable Long id) {
+    public ResponseEntity<?> findById(@PathVariable Long id) {
         MemberDto memberById = memberService.getMemberById(id);
-        ResponseDto<MemberDto> response = new ResponseDto<>("회원 조회 성공", memberById);
-        return ResponseEntity.ok().body(response);
+        return ResponseEntity.status(HttpStatus.OK).body(memberById);
     }
 
-    @PostMapping("/")
-    public ResponseEntity<ResponseDto<MemberDto>> createMember(@RequestBody MemberDto memberDto) {
+    @PostMapping("/register")  // POST /members 경로
+    public ResponseEntity<?> createMember(@RequestBody MemberDto memberDto) {
         MemberDto createdMember = memberService.createMember(memberDto);
-        ResponseDto<MemberDto> response = new ResponseDto<>("회원 생성 성공", createdMember);
-        return ResponseEntity.ok().body(response);
+        return ResponseEntity.status(HttpStatus.CREATED).body(createdMember);
     }
 
     @PutMapping("/{id}")
     public ResponseEntity<?> updateMember(@PathVariable Long id, @RequestBody MemberDto memberDto) {
-        MemberDto updateMember = memberService.updateMember(id, memberDto);
+        MemberDto updateMember = memberService.updateMember(id, memberDto.getPassword(), memberDto);
         MemberUpdateResponseDto responseDto = MemberUpdateResponseDto.toResponseDto(updateMember);
-        ResponseDto<MemberUpdateResponseDto> response = new ResponseDto<>("회원 update 성공", responseDto);
-        return ResponseEntity.ok().body(responseDto);
+        return ResponseEntity.status(HttpStatus.OK).body(responseDto);
     }
 
-    @GetMapping("/{email}")
-    public ResponseEntity<ResponseDto<MemberDto>> findByEmail(@PathVariable String email) {
+    @GetMapping("/email")
+    public ResponseEntity<?> findByEmail(@RequestParam String email) {
         MemberDto memberByEmail = memberService.getMemberByEmail(email);
-        ResponseDto<MemberDto> response = new ResponseDto<>("회원을 성공적으로 찾았습니다.", memberByEmail);
-        return ResponseEntity.ok().body(response);
+        return ResponseEntity.status(HttpStatus.OK).body(memberByEmail);
     }
 
     @DeleteMapping("/{id}")
-    public ResponseEntity<ResponseDto<Void>> deleteMemberById(@PathVariable Long id) {
+    public ResponseEntity<?> deleteMemberById(@PathVariable Long id) {
         memberService.deleteMemberById(id);
-        ResponseDto<Void> response = new ResponseDto<>("회원 탈퇴 성공", null);
-        return ResponseEntity.ok(response);
+        return ResponseEntity.status(HttpStatus.OK).body(null);
     }
 }
