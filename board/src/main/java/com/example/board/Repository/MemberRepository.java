38c89666d2c321a0699f2a52f23ba6ebe8f79a0a
JHabin <habin424@gmail.com>
2024-11-20 18:29:14 +0900

    Delete board/src/main/java directory

--- board/src/main/java/com/example/board/Repository/MemberRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.board.Repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.example.board.Entity.Member; // Member 클래스 패키지 경로도 확인
-
-public interface MemberRepository extends JpaRepository<Member, Long> {
-}
